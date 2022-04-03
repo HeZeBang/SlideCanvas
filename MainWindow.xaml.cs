e395da9f19a00dc4c7b48dddae17b21fa9ce7c60
HeZeBang <zambar@163.com>
2022-04-03 22:55:46 +0800

    Initialized the basic function.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,372 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using Windows.Networking.NetworkOperators;
+
+namespace SlideCanvas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PPTInterface ppt;
+        DispatcherTimer timer = new DispatcherTimer();
+        Stack<StrokeCollection> tempList = new Stack<StrokeCollection>();
+        List<StrokeCollection> slidestroke = new List<StrokeCollection>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            bdrPenSet.Visibility = Visibility.Collapsed;
+            this.sldSize.Minimum = InkCanvasMain.DefaultDrawingAttributes.Width;
+            var drawingAttributes = new DrawingAttributes()
+            {
+                StylusTip = StylusTip.Ellipse,
+                FitToCurve = true,
+                Width = this.sldSize.Value,
+                Height = this.sldSize.Value
+            };
+            drawingAttributes.StylusTip = StylusTip.Ellipse;
+            drawingAttributes.FitToCurve = true;
+            InkCanvasMain.DefaultDrawingAttributes = drawingAttributes;
+            ppt = new();
+            lblIP.Content = ppt.ip;
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(ppt.ip, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            System.Drawing.Bitmap ImageOriginalBase = qrCode.GetGraphic(20);
+            BitmapImage bitmapImage = new BitmapImage();
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+            {
+                ImageOriginalBase.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = ms;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+            }
+            bmpIP.Source = bitmapImage;
+            string[] pargs = Environment.GetCommandLineArgs();
+
+            Topmost = true;
+            timer.Tick += new EventHandler(timer_Tick);
+            if (pargs.Length > 1)
+            {
+                Task task = new(() =>
+                {
+                    ppt.OpenFile(pargs[1]);
+                    ppt.GetNum();
+                });
+                task.Start();
+                slidestroke.Clear();
+                tempList.Clear();
+                if (ppt.presentation != null)
+                { 
+                    timer.Start();
+                }
+            }
+            Excp("WELCOME", "TO SLIDECANVAS");
+            InkCanvasMain.Strokes.StrokesChanged += Strokes_StrokesChanged;
+        }
+
+        private void Strokes_StrokesChanged(object sender, System.Windows.Ink.StrokeCollectionChangedEventArgs e)
+        {
+            if (e.Added.Count > 0)
+            {
+                tempList.Push(e.Added);
+            }
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ppt.presentation != null)
+                {
+                    ppt.GetNum();
+                    if (btnPage.Content != ppt.info)
+                    {
+                        string[] val = ppt.info.Split("/");
+                        for (int i = 0; i < Convert.ToInt32(val[0]); i++)
+                            slidestroke.Add(new StrokeCollection());
+                        if (slidestroke[Convert.ToInt32(val[0])] == new StrokeCollection())
+                        {
+                            slidestroke[Convert.ToInt32(val[0])] = InkCanvasMain.Strokes;
+                            InkCanvasMain.Strokes.Clear();
+                        }
+                        else
+                        {
+                            InkCanvasMain.Strokes = slidestroke[Convert.ToInt32(val[0])];
+                        }
+                        btnPage.Content = ppt.info;
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void ClearCanvas(object sender, RoutedEventArgs e)
+        {
+            this.InkCanvasMain.Strokes.Clear();
+        }
+        private void CustomColor(object sender, RoutedEventArgs e)
+        {
+            var pck = new System.Windows.Forms.ColorDialog();
+            var dialogResult = pck.ShowDialog();
+            if (dialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                var drawingAttributes = this.InkCanvasMain.DefaultDrawingAttributes;
+                drawingAttributes.Color = Color.FromArgb(pck.Color.A, pck.Color.R, pck.Color.G, pck.Color.B);
+                brdCus.Background = new SolidColorBrush(drawingAttributes.Color);
+                radCus.IsChecked = true;
+                drawingAttributes.FitToCurve = true;
+                InkCanvasMain.DefaultDrawingAttributes = drawingAttributes;
+            }
+        }
+
+
+        private void SizeSet(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var drawingAttributes = this.InkCanvasMain.DefaultDrawingAttributes;
+            drawingAttributes.StylusTip = StylusTip.Ellipse;
+            this.InkCanvasMain.DefaultDrawingAttributes.Width = this.sldSize.Value;
+            this.InkCanvasMain.DefaultDrawingAttributes.Height = this.sldSize.Value;
+            InkCanvasMain.DefaultDrawingAttributes = drawingAttributes;
+        }
+
+
+        private void ToogleBtn(object sender, RoutedEventArgs e)
+        {
+            ToogleVisibility(bdrPenSet, Visibility.Collapsed);
+            ToogleVisibility(bdrSet, Visibility.Collapsed);
+            this.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#01FFFFFF"));
+            foreach (Border item in ToolPanel.Children)
+                item.Background = null;
+            switch ((sender as Button).Name)
+            {
+                case "btnSlc":
+                    this.InkCanvasMain.EditingMode = InkCanvasEditingMode.Select;
+                    break;
+                case "btnPen":
+                    if (this.InkCanvasMain.ActiveEditingMode == InkCanvasEditingMode.Ink)
+                    {
+                        bdrPenSet.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CCFFFFFF"));
+                        ToogleVisibility(bdrPenSet, Visibility.Visible);
+                    }
+                    this.InkCanvasMain.EditingMode = InkCanvasEditingMode.Ink;
+                    break;
+                case "btnEra":
+                    this.InkCanvasMain.EditingMode = InkCanvasEditingMode.EraseByPoint;
+                    break;
+                case "btnArr":
+                    this.Background = Brushes.Transparent;
+                    this.InkCanvasMain.EditingMode = InkCanvasEditingMode.Select;
+                    break;
+                default:
+                    break;
+            }
+            ((sender as Button).Parent as Border).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF4DFF4D"));
+        }
+        private void Window_Deactivated(object sender, System.EventArgs e)
+        {
+            Window window = (Window)sender;
+            window.Topmost = true;
+        }
+
+        private void ColorPick(object sender, RoutedEventArgs e)
+        {
+            var drawingAttributes = this.InkCanvasMain.DefaultDrawingAttributes;
+            drawingAttributes.StylusTip = StylusTip.Ellipse;
+            bdrPenSet.Background = ((sender as RadioButton).Parent as Border).Background;
+            drawingAttributes.Color = (Color)ColorConverter.ConvertFromString(((sender as RadioButton).Parent as Border).Background.ToString());
+            InkCanvasMain.DefaultDrawingAttributes = drawingAttributes;
+        }
+        private void ClsPanel(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ToogleVisibility((sender as Border), Visibility.Collapsed);
+        }
+
+        private void CanvasFoc(object sender, RoutedEventArgs e)
+        {
+            ToogleVisibility(bdrPenSet, Visibility.Collapsed);
+        }
+        private void ToogleVisibility(Border character, Visibility visibility)
+        {
+            Storyboard storyboard = new();
+            if (visibility == Visibility.Visible)
+            {
+                character.Visibility = visibility;
+                storyboard = (FindResource("FadeIn") as System.Windows.Media.Animation.Storyboard);
+            }
+            else
+                storyboard = (FindResource("FadeOut") as System.Windows.Media.Animation.Storyboard);
+            storyboard.SpeedRatio = 1;
+            storyboard.Completed += (o, a) => { character.Visibility = visibility; };
+            storyboard.Begin(character);
+        }
+        private void ToogleVisibility(Border character)
+        {
+            Storyboard storyboard = new(), storyboard1 = new();
+            character.Visibility = Visibility.Visible;
+            storyboard = (FindResource("FadeIn") as System.Windows.Media.Animation.Storyboard);
+            storyboard.SpeedRatio = 1;
+            storyboard1 = (FindResource("FadeOutSlow") as System.Windows.Media.Animation.Storyboard);
+            storyboard.Completed += (o, a) =>
+            {
+                storyboard1.Begin(character);
+            };
+            storyboard1.Completed += (o, a) => { character.Visibility = Visibility.Collapsed; };
+            storyboard.Begin(character);
+        }
+
+        private void PageClick(object sender, RoutedEventArgs e)
+        {
+            if (ppt.presentation == null)
+            {
+                var openFileDialog = new System.Windows.Forms.OpenFileDialog();
+                openFileDialog.Filter = "PowerPoint|*.ppt;*.pptx;*.pptm|所有文件|*.*";
+                var result = openFileDialog.ShowDialog();
+                if (openFileDialog.FileName != null)
+                {
+                    try
+                    {
+                        string filename = openFileDialog.FileName;
+                        ppt.OpenFile(filename);
+                        ppt.FetchInnerText();
+                    }
+                    catch (Exception ex)
+                    {
+                        Excp("哦吼", ex.Message);
+                    }
+                }
+                slidestroke.Clear();
+                tempList.Clear();
+                timer.Start();
+            }
+            else
+            {
+                if (!ppt.Zoom(20))
+                    Excp("Error", "打开缩略图失败");
+                ToogleBtn(btnArr, e);
+            }
+        }
+
+        private void PageChange(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                switch ((sender as Button).Name)
+                {
+                    case "btnPrev":
+                        if (!ppt.Prev())
+                            Excp("啊？", "请先打开一个PPT");
+                        break;
+                    case "btnNext":
+                        if (!ppt.Next())
+                            Excp("啊？", "请先打开一个PPT");
+                        break;
+                    default:
+                        break;
+                }
+                if (ppt.presentation != null)
+                    ppt.GetNum();
+                btnPage.Content = ppt.info;
+            }
+            catch (Exception ex)
+            {
+                Excp("Error", ex.ToString());
+            }
+        }
+        private void Excp(string title, string content)
+        {
+            Border bdr = bdrInfo;
+            ((bdr.Child as StackPanel).Children[0] as Label).Content = title;
+            (((bdr.Child as StackPanel).Children[1] as Border).Child as Label).Content = content;
+            ToogleVisibility(bdr);
+
+        }
+
+        private void CloseWindow(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ppt.Shut();
+        }
+
+        private void SetShow(object sender, RoutedEventArgs e)
+        {
+            if (bdrSet.Visibility == Visibility.Visible)
+                ToogleVisibility(bdrSet, Visibility.Collapsed);
+            else
+                ToogleVisibility(bdrSet, Visibility.Visible);
+        }
+
+
+        private async void ToggleWLAN(object sender, RoutedEventArgs e)
+        {
+            if (swcSpot.IsOn)
+            {
+                grdSpot.Visibility = Visibility.Visible;
+                lblSpot.Content = "Launching...";
+                var connectionProfile = Windows.Networking.Connectivity.NetworkInformation.GetInternetConnectionProfile();
+                var tetheringManager = Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager.CreateFromConnectionProfile(connectionProfile);
+                var access = tetheringManager.GetCurrentAccessPointConfiguration();
+                access.Ssid = System.Net.Dns.GetHostName() + " - SlideCanvas";
+                access.Passphrase = "SlideCanvas";
+                var result = await tetheringManager.StartTetheringAsync();
+                if (result.Status == TetheringOperationStatus.Success)
+                {
+                    lblSpot.Content = string.Format("SSID: {0}\nPwd: {1}", access.Ssid, access.Passphrase);
+                    QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                    QRCodeData qrCodeData = qrGenerator.CreateQrCode(string.Format("WIFI:T:WPA;S:{0};P:{1};;", access.Ssid, access.Passphrase), QRCodeGenerator.ECCLevel.Q);
+                    QRCode qrCode = new QRCode(qrCodeData);
+                    System.Drawing.Bitmap ImageOriginalBase = qrCode.GetGraphic(20);
+                    BitmapImage bitmapImage = new BitmapImage();
+                    using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+                    {
+                        ImageOriginalBase.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                        bitmapImage.BeginInit();
+                        bitmapImage.StreamSource = ms;
+                        bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmapImage.EndInit();
+                        bitmapImage.Freeze();
+                    }
+                    bmpSpot.Source = bitmapImage;
+                }
+                else
+                {
+                    lblSpot.Content = "Failed to Launch.";
+                }
+            }
+            else
+            {
+                var connectionProfile = Windows.Networking.Connectivity.NetworkInformation.GetInternetConnectionProfile();
+                var tetheringManager = Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager.CreateFromConnectionProfile(connectionProfile);
+                var result = await tetheringManager.StopTetheringAsync();
+                if (result.Status == TetheringOperationStatus.Success)
+                    grdSpot.Visibility = Visibility.Collapsed;
+                else
+                    lblSpot.Content = "Failed to Shut.";
+                grdSpot.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Undo(object sender, RoutedEventArgs e)
+        {
+            if (tempList.Count > 0)
+                InkCanvasMain.Strokes.Remove(tempList.Pop());
+            else
+                Excp("咩？", "已经到底了（＞人＜；）");
+        }
+    }
+}
---
+++ PPTInterface.cs
@@ -0,0 +1,324 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+using Zack.ComObjectHelpers;
+
+namespace SlideCanvas
+{
+    /* Github Reference
+     * https://github.com/yangzhongke/PhoneAsPrompter
+     * https://github.com/PuZhiweizuishuai/PPT-Remote-control
+     */
+    internal class PPTInterface
+    {
+        internal const int port = 7999;
+
+        private IWebHost webHost;
+
+        internal dynamic presentation;
+
+        internal string ip = "", page = "", info = "";
+
+        internal BitmapImage bim;
+
+        private COMReferenceTracker comReference = new COMReferenceTracker();
+
+        private static readonly string AUTH_KEY = "auth";
+
+        private static Hashtable user = new Hashtable();
+        internal PPTInterface()
+        {
+            if (!NetworkInterface.GetIsNetworkAvailable())
+            {
+                MessageBox.Show("网络未连接，程序可能会运行异常，请连接网络后重试！");
+            }
+            // 显示IP
+            ShowUrl();
+            // 配置服务器
+            this.webHost = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(ConfigureWebApp)
+                .UseUrls("http://*:" + port)
+                .Build();
+
+            // 异步运行服务器
+            this.webHost.RunAsync();
+
+
+            // 关闭窗口处理
+            // this.FormClosed += Form1_FormClosed;
+        }
+        internal void Shut()
+        {
+            // 关闭所有 COM　对象，以及当前打开的PPT
+            ClearComRefs();
+            // 停止运行服务器
+            this.webHost.StopAsync();
+            this.webHost.WaitForShutdown();
+            Process.GetCurrentProcess().Kill();
+        }
+        private void ConfigureWebApp(IApplicationBuilder app)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Run(async (context) =>
+            {
+                // 处理非静态请求 
+                var request = context.Request;
+                var response = context.Response;
+                string path = request.Path.Value;
+                response.ContentType = "application/json; charset=UTF-8";
+                bool hasRun = true;
+
+                IsAuth(request.Cookies, response);
+
+                if (path == "/api/report")
+                {
+                    string value = request.Query["value"];
+                    page = value;
+                    response.StatusCode = 200;
+                    await response.WriteAsync("ok");
+                }
+                else if (path == "/api/getNote")
+                {
+                    string notesText = "";
+                    if (this.presentation == null)
+                    {
+                        return;
+                    }
+                    try
+                    {
+                        dynamic notesPage = T(T(T(T(presentation.SlideShowWindow).View).Slide).NotesPage);
+                        notesText = GetInnerText(notesPage);
+                    }
+                    catch (COMException ex)
+                    {
+                        notesText = "";
+                    }
+                    await response.WriteAsync(notesText);
+                }
+                else if (path == "/api/next")
+                {
+                    response.StatusCode = 200;
+                    if (this.presentation == null)
+                    {
+                        return;
+                    }
+                    try
+                    {
+                        T(T(this.presentation.SlideShowWindow).View).Next();
+                        GetNum();
+                        hasRun = true;
+                    }
+                    catch (COMException e)
+                    {
+                        hasRun = false;
+                    }
+
+
+
+                    if (hasRun)
+                    {
+                        await response.WriteAsync("OK");
+                    }
+                    else
+                    {
+                        await response.WriteAsync("NO");
+                    }
+                }
+                else if (path == "/api/previous")
+                {
+                    response.StatusCode = 200;
+                    if (this.presentation == null)
+                    {
+                        return;
+                    }
+                    try
+                    {
+                        T(T(this.presentation.SlideShowWindow).View).Previous();
+                        GetNum();
+                        hasRun = true;
+                    }
+                    catch (COMException e)
+                    {
+                        hasRun = false;
+                    }
+
+                    if (hasRun)
+                    {
+                        await response.WriteAsync("OK");
+                    }
+                    else
+                    {
+                        await response.WriteAsync("NO");
+                    }
+
+                }
+                else
+                {
+                    response.StatusCode = 404;
+                }
+            });
+
+        }
+        internal void ShowUrl()
+        {
+            this.ip = "http://" + GetIPUtil.IPV4() + ":" + port;
+            return;
+        }
+        private bool IsAuth(IRequestCookieCollection cookies, HttpResponse response)
+        {
+            foreach (var cookie in cookies)
+            {
+                if (cookie.Key == AUTH_KEY)
+                {
+                    if (user.ContainsKey(cookie.Value))
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            CookieOptions cookieOptions = new();
+            cookieOptions.HttpOnly = true;
+            cookieOptions.Path = "/";
+            response.Cookies.Append(AUTH_KEY, Guid.NewGuid().ToString("N"), cookieOptions);
+            return false;
+
+        }
+        private void ClearComRefs()
+        {
+            try
+            {
+                if (this.presentation != null)
+                {
+                    T(this.presentation.Application).Quit();
+                    this.presentation = null;
+                }
+            }
+            catch (COMException ex)
+            {
+                Debug.WriteLine(ex);
+                this.presentation = null;
+            }
+            this.comReference.Dispose();
+            this.comReference = new COMReferenceTracker();
+        }
+
+        private dynamic T(dynamic comObj)
+        {
+            return this.comReference.T(comObj);
+        }
+
+        internal void OpenFile(string filename)
+        {
+            this.ClearComRefs();
+            dynamic pptApp = T(PowerPointHelper.CreatePowerPointApplication());
+            pptApp.Visible = true;
+            dynamic presentations = T(pptApp.Presentations);
+            this.presentation = T(presentations.Open(filename));
+            T(this.presentation.SlideShowSettings).Run();
+        }
+        private string GetInnerText(dynamic part)
+        {
+            StringBuilder sb = new StringBuilder();
+            dynamic shapes = T(T(part).Shapes);
+            int shapesCount = shapes.Count;
+            for (int i = 0; i < shapesCount; i++)
+            {
+                dynamic shape = T(shapes[i + 1]);
+                var textFrame = T(shape.TextFrame);
+                // MsoTriState.msoTrue==-1
+                if (textFrame.HasText == -1)
+                {
+                    string text = T(textFrame.TextRange).Text;
+                    sb.AppendLine(text);
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+        internal void FetchInnerText()
+        {
+            if (this.presentation == null)
+            {
+                MessageBox.Show("请先选择打开一个PPT文件");
+                return;
+            }
+
+            dynamic notesPage = T(T(T(T(presentation.SlideShowWindow).View).Slide).NotesPage);
+            string notesText = GetInnerText(notesPage);
+        }
+        
+        internal void GetNum()
+        {
+            if (this.presentation == null)
+            {
+                info = "&#xefc8;\n导入";
+            }
+            try
+            {
+                dynamic cur = T(T(T(presentation.SlideShowWindow).View).Slide).SlideIndex;
+                dynamic tot = T(presentation.Slides).Count;
+                info = string.Format("{0}/{1}", cur, tot);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+        }
+        internal bool Prev()
+        {
+            if (this.presentation == null)
+            {
+                return false;
+            }
+            T(T(presentation.SlideShowWindow).View).Previous();
+            return true;
+        }
+        internal bool Next()
+        {
+            if (this.presentation == null)
+            {
+                return false;
+            }
+            T(T(presentation.SlideShowWindow).View).Next();
+            return true;
+        }
+        internal bool Zoom(int index)
+        {
+            if (this.presentation == null)
+            {
+                return false;
+            }
+            if (index >= 10 && index <= 400)
+                try
+                {
+                    T(T(presentation.SlideShowWindow).View).Zoom = 30;
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                    try
+                    {
+                        presentation.SlideShowWindow.Activate();
+                        SendKeys.SendWait("G");
+                    }catch (Exception ex2)
+                    {
+                        System.Diagnostics.Debug.WriteLine(ex2.Message);
+                    }
+                }
+            return true;
+        }
+    }
+}
